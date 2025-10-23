8d2a0e73d80bbf70a4a9693fb678e36a40122b37
Maria Luisa Campos <marialuisacampos@MacBook-Air-de-Maria.local>
2025-10-22 22:04:42 -0300

    fix: change to nodejs runtime

--- app/u/[username]/blog/[slug]/opengraph-image.tsx
+++ app/u/[username]/blog/[slug]/opengraph-image.tsx
@@ -1,7 +1,7 @@
 import { ImageResponse } from "next/og";
 import { getUserPost } from "@/lib/db/posts";
 
-export const runtime = "edge";
+export const runtime = "nodejs";
 export const alt = "Post Cover";
 export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
